df82260cd57c6da78c48ba12de056513b1a26cfd
Hoa Luu <hoamluu@gmail.com>
2018-09-30 23:39:23 -0400

    added cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 
 const profiles = require('./api/profiles');
 
